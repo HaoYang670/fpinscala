74be1c1a07364926eeb4ac123bb46103beac0ec1
remzi <13716567376yh@gmail.com>
2023-01-23 18:16:18 +0800

    concat
    Signed-off-by: remzi <13716567376yh@gmail.com>

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -98,7 +98,8 @@ object List: // `List` companion object. Contains functions for creating and wor
   def appendViaFoldRight[A](l: List[A], r: List[A]): List[A] =
     foldRight(l, r, (ln, acc) => Cons(ln, acc))
 
-  def concat[A](l: List[List[A]]): List[A] = ???
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, Nil: List[A], (ls, acc) => appendViaFoldRight(ls, acc))
 
   def incrementEach(l: List[Int]): List[Int] = ???
 
